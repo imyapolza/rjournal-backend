673859969abfd750510e20737fda981557de483d
Viner Gataullin <94351574+imyapolza@users.noreply.github.com>
2022-09-14 01:50:03 +0500

    dto post update added

--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostDto } from './create-post.dto';
+import { PartialType } from "@nestjs/mapped-types";
+import { CreatePostDto } from "./create-post.dto";
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto {
+  title?: string;
+  body?: string;
+}
